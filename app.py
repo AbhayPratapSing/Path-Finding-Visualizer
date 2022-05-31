f720b6ef745c963d0d92b789d9113b9fb4efc445
AbhayPratapSing <sabhay083@gmail.com>
2022-05-31 20:27:22 +0530

    abhay

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.image('maze1.jpg')
 #st.subheader('Tag Line')
 
 
-Astr(WIDTH)
+
 def overview():
     st.header('Path Finding Algorithms')
     st.markdown('Path finding algorithms build on top of graph search algorithms and explore routes between nodes, starting at one node and traversing through relationships until the destination has been reached. These algorithms find the cheapest path in terms of the number of hops or weight.', unsafe_allow_html=False)
@@ -51,7 +51,7 @@ def View_Algorithm():
     st.header('3. Swarm Algorithm')
     st.header('4. Breadth-first Search')
     st.header('5. Depth-first Search') 
-    
+
 def Tool_Used():
     st.header('Developer')
     st.text('* Web Browser (Google Chrome, Firefox )')
